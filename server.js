4ddd1aaba034eb5834ea2078dfef39a88a6d132f
Romit Tiwari <romittiwari100@gmail.com>
2025-07-02 18:12:55 +0530

    new year condition added

--- server.js
+++ server.js
@@ -20,6 +20,86 @@ try {
     const yy = String(date.getFullYear()).slice(-2);
     return `${mm}/${dd}/${yy}`;
   };
+  const finishNewDate = (startDate) => {
+    let currYear = Number(startDate?.split("/")[2]);
+    let finishYear = currYear + 1;
+    let finishDate = `${startDate?.split("/")[0]}/${
+      startDate?.split("/")[1]
+    }/${finishYear}`;
+    return finishDate;
+  };
+  const checkNumber = (currChar) => {
+    if (
+      currChar == "0" ||
+      currChar == "1" ||
+      currChar == "2" ||
+      currChar == "3" ||
+      currChar == "4" ||
+      currChar == "5" ||
+      currChar == "6" ||
+      currChar == "7" ||
+      currChar == "8" ||
+      currChar == "9"
+    ) {
+      return true;
+    }
+    return false;
+  };
+  const tempYear = (submissionNameString) => {
+    let currYear = "";
+    for (let currIdx = 0; currIdx < submissionNameString?.length; currIdx++) {
+      if (submissionNameString[currIdx] === "-") {
+        if (submissionNameString.length >= currIdx + 7) {
+          break;
+        }
+        if (
+          submissionNameString[currIdx + 1] === " " &&
+          submissionNameString[currIdx + 6] === " " &&
+          submissionNameString[currIdx + 7] === "-"
+        ) {
+          let yearString = "";
+          if (
+            checkNumber(submissionNameString[currIdx + 2]) &&
+            checkNumber(submissionNameString[currIdx + 3]) &&
+            checkNumber(submissionNameString[currIdx + 3]) &&
+            checkNumber(submissionNameString[currIdx + 4])
+          ) {
+            yearString += submissionNameString[currIdx + 2];
+            yearString += submissionNameString[currIdx + 3];
+            yearString += submissionNameString[currIdx + 4];
+            yearString += submissionNameString[currIdx + 5];
+            if (currYear === "") {
+              currYear = yearString;
+            }
+          }
+        } else {
+          break;
+        }
+      }
+    }
+    return currYear;
+  };
+  for (let i = 0; i < responsesArray.length; i++) {
+    let submissionNameString = getValue(responsesArray[i]?.name);
+    let currYear = "";
+    // have to find - xxxx -  pattern in submission string
+    if (
+      getValue(responsesArray[i]?.duration?.start) === "-" &&
+      getValue(responsesArray[i]?.duration?.end) === "-"
+    ) {
+      currYear = tempYear(submissionNameString);
+      if (currYear?.length === 4) {
+        responsesArray[i].duration.end = `01/01/${currYear + 1}`;
+        responsesArray[i].duration.start = `01/01/${currYear}`;
+      }
+    } else {
+      let startDate = getValue(responsesArray[i]?.duration?.start);
+      if (getValue(responsesArray[i]?.duration?.end) === "-") {
+        let newFinishDate = finishNewDate(startDate);
+        responsesArray[i].duration.end = newFinishDate;
+      }
+    }
+  }
   const submissions = responsesArray.map((item) => ({
     "Organization Legal Name": getValue(item?.nonprofit?.legalName),
     "Grant Submission Name": getValue(item?.name),
